578f84a87d38ade4877a63e16482b77b87989e72
yara30999 <yaranaserelniny@gmail.com>
2025-01-16 03:44:26 +0200

    called getEmployee method in home screen & cache it

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:sprints_user_profile_management/models/employee.dart';
 import 'package:sprints_user_profile_management/screens/add_user_screen.dart';
+import 'package:sprints_user_profile_management/services/api_service.dart';
+import 'package:sprints_user_profile_management/services/shared_prefs_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   /*
       1 create pull to refresh using any package you want
-          (when you pull to refresh , two actions will happen [remove the cache] & [getEmployees() method again]) 
+          (when you pull to refresh , two actions will happen first [remove the cache] then [getEmployees() method again]) 
       2 when the app opened you need to call  getEmployees() in initState and show it here in lists
       3 each employee list can swip to right or left , for [delete employee] and [edit employee]
       4 show snack bar after deleting 
 
       you any packages you want ;) 
   */
+
+  late Future<List<Employee>> employees;
+  final ApiService service = ApiService();
+
+  @override
+  void initState() {
+    super.initState();
+    getEmployeeData();
+  }
+
+  Future<void> getEmployeeData() async {
+    //if the cache is empty call the api
+    employees = AppPreferences.loadCachedEmployees().then((cached) {
+      if (cached.isNotEmpty) {
+        return cached;
+      }
+      return service.getEmployees();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
